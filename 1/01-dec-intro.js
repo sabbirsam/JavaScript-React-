b52e00ccb7940672a82862fab1e01be85da4daf1
sabbirsam <samiubat14@gmail.com>
2021-12-16 12:28:59 +0600

    first commit

---
+++ 1/01-dec-intro.js
@@ -0,0 +1,15 @@
+// let s = "sabbir";
+// let a = 16;
+
+// let c = `ok ${s} how are you and age is ${a}`;
+// console.log(c);
+
+
+let price = 10;
+let VAT = 0.25;
+
+// let total = `Total: ${(price * (1 + VAT)).toFixed(2)}`;
+
+var total = `Total: ${price + (1+ VAT)}`;
+// console.log(total);
+console.log(total);
