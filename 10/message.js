5513088391a14237a04da7a29da1ff7b3a61a21f
sabbirsam <samiubat14@gmail.com>
2022-02-08 21:59:06 +0600

    complete module

---
+++ 10/message.js
@@ -0,0 +1,9 @@
+const message = () => {
+    const name = "Sabbir ahmed";
+    const age = 14;
+
+    return name + " " + age;
+
+};
+
+export default message;
---
+++ 10/person.js
@@ -0,0 +1,15 @@
+// export const name = "Jesse";
+// export const age = 40;
+
+// or  
+
+/**
+const name = "sam";
+const age = 14;
+
+export { name, age };
+ */
+const name = "sam";
+const age = 14;
+
+export { name, age };
