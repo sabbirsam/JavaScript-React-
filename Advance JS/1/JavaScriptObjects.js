73a990671f3fedf357b1d303939ecce814c678ce
sabbirsam <samiubat14@gmail.com>
2022-02-10 20:22:28 +0600

    Start advanced JavaScript and begain with Object

---
+++ Advance JS/1/JavaScriptObjects.js
@@ -0,0 +1,30 @@
+// Objects are Variables
+let person = "John Doe";
+let person = { name: "Name", age: 13, address: "dhaka" };
+const person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+};
+
+
+const person = {};
+person.name = "Sabbir";
+person.age = 13;
+
+// Using the JavaScript Keyword new
+
+const person = new Object();
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
+
+
+
+/**
+ * JavaScript Object Properties
+ */
