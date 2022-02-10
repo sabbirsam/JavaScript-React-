62d93f49e359433040874506f15d728509e42ec5
sabbirsam <samiubat14@gmail.com>
2022-02-10 15:55:53 +0600

    Object.defineProperty()

--- 12-debug/backup.js
+++ 12-debug/backup.js
@@ -1,1 +1,79 @@
 // https: //www.w3schools.com/js/js_versions.asp
+
+// Getter 
+var person = {
+    name: "sabbir",
+    age: 15,
+    get Info() {
+        return this.name + " " + this.age;
+    }
+}
+
+console.log(person.Info);
+
+
+
+// setter
+
+var person = {
+    name: "sabbir",
+    age: 15,
+    get updateInfo() {
+        return this.name;
+    },
+
+    set updateInfo(n) {
+        this.name = n;
+
+
+    }
+
+};
+
+person.updateInfo = "sam";
+
+
+
+console.log(person.name); //sam
+
+
+var person = {
+    name: "sabbir",
+    age: 15,
+    get updateInfo() {
+        return this.name;
+    },
+
+    set updateInfo(n) {
+        this.name = n.toUpperCase();
+
+
+    }
+
+};
+
+person.updateInfo = "sam";
+
+
+
+console.log(person.name); //SAM
+
+
+
+
+var person = {
+    name: "sabbir",
+    age: 15,
+};
+
+// Object.defineProperties(person, "name", {});
+Object.defineProperty(person, "name", {
+    value: "Ahmed",
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
+
+Object.defineProperty()
+
+console.log(person.name); //Ahmed
--- 12-debug/script.js
+++ 12-debug/script.js
@@ -1,1 +1,16 @@
-// https://www.w3schools.com/js/js_versions.asp
+var person = {
+    name: "sabbir",
+    age: 15,
+};
+
+// Object.defineProperties(person, "name", {});
+Object.defineProperty(person, "name", {
+    value: "Ahmed",
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
+
+
+
+console.log(person.name); //Ahmed
